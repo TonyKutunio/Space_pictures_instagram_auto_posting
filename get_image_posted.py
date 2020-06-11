d38a778fd02f96521a1edd2f3b79eb69202fbc1a
TonyKutunio <55698444+TonyKutunio@users.noreply.github.com>
2020-06-11 23:23:42 +0100

    Used Pathlib

--- get_image_posted.py
+++ get_image_posted.py
@@ -3,25 +3,23 @@ import instabot
 from instabot import Bot
 from os import listdir
 from dotenv import load_dotenv
-from functools import partial
+from pathlib import Path
 
 
 def get_image_posted(username, password, folder_path):
-    folder_with_pictures = listdir(folder_path)
+    folder_with_picture_names = listdir(folder_path)
     bot = instabot.Bot()
     bot.login(username=username, password=password)
 
-    for picture in folder_with_pictures:
-        bot.upload_photo(folder_path + picture)
+    for picture_name in folder_with_picture_names:
+        bot.upload_photo(folder_path + picture_name)
 
 if __name__ == "__main__":
     load_dotenv()
     username = os.getenv('INSTAGRAM_USERNAME')
     password = os.getenv('INSTAGRAM_PASSWORD')
-    folder_path = ('Instagram_auto_posting/Space Pictures/')
-    get_image_posted_partial = partial(get_image_posted,username, password, folder_path)
-    get_image_posted_partial()
-
+    folder_path = (Path.cwd() / 'Space Pictures')
+    get_image_posted(username, password, folder_path)
 
 
 
